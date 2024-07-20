e826109d5374ccb1154e471a926e8b41423827ed
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-07-20 09:36:10 +0200

    ✨ uuid region logic

---
+++ EsPublico.Kata.Orders.Tests/Domain/Models/RegionShould.cs
@@ -0,0 +1,32 @@
+using EsPublico.Kata.Orders.Domain;
+using EsPublico.Kata.Orders.Domain.Models;
+using FluentAssertions;
+
+namespace EsPublico.Kata.Orders.Tests.Domain.Models;
+
+public class RegionShould
+{
+    [Fact]
+    public void BeCreated()
+    {
+        const string aRegion = "Europe";
+        var validRegion = Region.Create(aRegion);
+
+        validRegion.Match(
+            value => value.ToString().Should().Be(aRegion),
+            error => error.Should().BeNull()
+        );
+    }
+
+    [Fact]
+    public void NotAllowNulls()
+    {
+        var invalidRegion = Region.Create(null);
+
+        invalidRegion.Match(
+            value => value.Should().BeNull(),
+            error => error.Should().BeOfType<MissingParameter>()
+                .Which.Message.Should().Be("Missing Region")
+        );
+    }
+}
---
+++ EsPublico.Kata.Orders/Domain/Models/Region.cs
@@ -0,0 +1,21 @@
+using LanguageExt;
+
+namespace EsPublico.Kata.Orders.Domain.Models;
+
+public class Region
+{
+    private readonly string _value;
+    private Region(string givenRegion) => _value = givenRegion;
+
+    public static Either<Error, Region> Create(string givenRegion)
+    {
+        if (string.IsNullOrWhiteSpace(givenRegion))
+        {
+            return new MissingParameter("Missing Region");
+        }
+
+        return new Region(givenRegion);
+    }
+
+    public override string ToString() => _value;
+}
