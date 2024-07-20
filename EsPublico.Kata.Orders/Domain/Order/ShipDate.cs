139a80c212db831f23c53d69bba37d348d51a927
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-07-20 10:15:23 +0200

    ✨ ship date domain logic

---
+++ EsPublico.Kata.Orders/Domain/Order/ShipDate.cs
@@ -0,0 +1,21 @@
+using LanguageExt;
+
+namespace EsPublico.Kata.Orders.Domain.Order;
+
+public class ShipDate
+{
+    private readonly DateTime _value;
+    private ShipDate(DateTime givenDate) => _value = givenDate;
+
+    public static Either<Error, ShipDate> Create(string givenDate)
+    {
+        if (!DateTime.TryParse(givenDate, out var date))
+        {
+            return new InvalidParameter("Invalid Date");
+        }
+
+        return new ShipDate(date);
+    }
+
+    public override string ToString() => _value.ToString("dd-MM-yyyy");
+}
