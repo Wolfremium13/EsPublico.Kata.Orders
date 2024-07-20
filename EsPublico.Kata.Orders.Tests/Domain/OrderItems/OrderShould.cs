2df1476b0231f02ba770853ec2dc901d5f95779a
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-07-20 11:40:43 +0200

    ✨ total order domain logic

---
+++ EsPublico.Kata.Orders.Tests/Domain/OrderItems/OrderShould.cs
@@ -0,0 +1,99 @@
+using EsPublico.Kata.Orders.Domain;
+using EsPublico.Kata.Orders.Domain.OrderItems;
+using FluentAssertions;
+
+namespace EsPublico.Kata.Orders.Tests.Domain.OrderItems;
+
+public class OrderShould
+{
+    private const long ValidId = 443368995;
+    private const string ValidRegion = "Sub-Saharan Africa";
+    private const string ValidCountry = "South Africa";
+    private const string ValidItemType = "Fruits";
+    private const string ValidSalesChannel = "Offline";
+    private const string ValidPriority = "M";
+    private const string ValidDate = "7/27/2012";
+    private const string ValidShipDate = "7/28/2012";
+    private const long ValidUnitsSold = 1593;
+    private const decimal ValidUnitPrice = 9.33m;
+    private const decimal ValidUnitCost = 6.92m;
+    private const decimal ValidTotalRevenue = 14862.69m;
+    private const decimal ValidTotalCost = 11023.56m;
+    private const decimal ValidTotalProfit = 3839.13m;
+
+    [Fact]
+    public void BeCreated()
+    {
+        const string invalidUuid = "invalid-uuid";
+
+        var invalidOrder = Order.Create(
+            invalidUuid,
+            ValidId,
+            ValidRegion,
+            ValidCountry,
+            ValidItemType,
+            ValidSalesChannel,
+            ValidPriority,
+            ValidDate,
+            ValidShipDate,
+            ValidUnitsSold,
+            ValidUnitPrice,
+            ValidUnitCost,
+            ValidTotalRevenue,
+            ValidTotalCost,
+            ValidTotalProfit
+        );
+
+        invalidOrder.Match(
+            order => order.Should().BeNull(),
+            error => error.Should().BeOfType<InvalidParameter>()
+                .Which.Message.Should().Be("Invalid UUID")
+        );
+    }
+
+    [Fact]
+    public void NotAllowWrongAnyWrongParameter()
+    {
+        const string validUuid = "1858f59d-8884-41d7-b4fc-88cfbbf00c53";
+
+        var validOrder = Order.Create(
+            validUuid,
+            ValidId,
+            ValidRegion,
+            ValidCountry,
+            ValidItemType,
+            ValidSalesChannel,
+            ValidPriority,
+            ValidDate,
+            ValidShipDate,
+            ValidUnitsSold,
+            ValidUnitPrice,
+            ValidUnitCost,
+            ValidTotalRevenue,
+            ValidTotalCost,
+            ValidTotalProfit
+        );
+
+        validOrder.Match(
+            order =>
+            {
+                order.Uuid.ToString().Should().Be(validUuid);
+                order.Id.Value.Should().Be(ValidId);
+                order.Region.ToString().Should().Be(ValidRegion);
+                order.Country.ToString().Should().Be(ValidCountry);
+                order.ItemType.ToString().Should().Be(ValidItemType);
+                order.SalesChannel.ToString().Should().Be(ValidSalesChannel);
+                order.Priority.ToString().Should().Be(ValidPriority);
+                order.Date.ToString().Should().Be("27-07-2012");
+                order.ShipDate.ToString().Should().Be("28-07-2012");
+                order.UnitsSold.Value.Should().Be(ValidUnitsSold);
+                order.UnitPrice.Value.Should().Be(ValidUnitPrice);
+                order.UnitCost.Value.Should().Be(ValidUnitCost);
+                order.TotalRevenue.Value.Should().Be(ValidTotalRevenue);
+                order.TotalCost.Value.Should().Be(ValidTotalCost);
+                order.TotalProfit.Value.Should().Be(ValidTotalProfit);
+            },
+            error => error.Should().BeNull()
+        );
+    }
+}
---
+++ EsPublico.Kata.Orders/Domain/OrderItems/Order.cs
@@ -0,0 +1,112 @@
+using EsPublico.Kata.Orders.Domain.Order;
+using LanguageExt;
+
+namespace EsPublico.Kata.Orders.Domain.OrderItems;
+
+public class Order
+{
+    private Order(
+        Uuid uuid,
+        Id id,
+        Region region,
+        Country country,
+        ItemType itemType,
+        SalesChannel salesChannel,
+        Priority priority,
+        Date date,
+        ShipDate shipDate,
+        UnitsSold unitsSold,
+        UnitPrice unitPrice,
+        UnitCost unitCost,
+        TotalRevenue totalRevenue,
+        TotalCost totalCost,
+        TotalProfit totalProfit
+    )
+    {
+        Uuid = uuid;
+        Id = id;
+        Region = region;
+        Country = country;
+        ItemType = itemType;
+        SalesChannel = salesChannel;
+        Priority = priority;
+        Date = date;
+        ShipDate = shipDate;
+        UnitsSold = unitsSold;
+        UnitPrice = unitPrice;
+        UnitCost = unitCost;
+        TotalRevenue = totalRevenue;
+        TotalCost = totalCost;
+        TotalProfit = totalProfit;
+    }
+
+    public Uuid Uuid { get; }
+    public Id Id { get; }
+    public Region Region { get; }
+    public Country Country { get; }
+    public ItemType ItemType { get; }
+    public SalesChannel SalesChannel { get; }
+    public Priority Priority { get; }
+    public Date Date { get; }
+    public ShipDate ShipDate { get; }
+    public UnitsSold UnitsSold { get; }
+    public UnitPrice UnitPrice { get; }
+    public UnitCost UnitCost { get; }
+    public TotalRevenue TotalRevenue { get; }
+    public TotalCost TotalCost { get; }
+    public TotalProfit TotalProfit { get; }
+
+
+    public static Either<Error, Order> Create(
+        string uuid,
+        long id,
+        string region,
+        string country,
+        string itemType,
+        string salesChannel,
+        string priority,
+        string date,
+        string shipDate,
+        long unitsSold,
+        decimal unitPrice,
+        decimal unitCost,
+        decimal totalRevenue,
+        decimal totalCost,
+        decimal totalProfit
+    )
+    {
+        return
+            from uuidValue in Uuid.Create(uuid)
+            from idValue in Id.Create(id)
+            from regionValue in Region.Create(region)
+            from countryValue in Country.Create(country)
+            from itemTypeValue in ItemType.Create(itemType)
+            from salesChannelValue in SalesChannel.Create(salesChannel)
+            from priorityValue in Priority.Create(priority)
+            from dateValue in Date.Create(date)
+            from shipDateValue in ShipDate.Create(shipDate)
+            from unitsSoldValue in UnitsSold.Create(unitsSold)
+            from unitPriceValue in UnitPrice.Create(unitPrice)
+            from unitCostValue in UnitCost.Create(unitCost)
+            from totalRevenueValue in TotalRevenue.Create(totalRevenue)
+            from totalCostValue in TotalCost.Create(totalCost)
+            from totalProfitValue in TotalProfit.Create(totalProfit)
+            select new Order(
+                uuidValue,
+                idValue,
+                regionValue,
+                countryValue,
+                itemTypeValue,
+                salesChannelValue,
+                priorityValue,
+                dateValue,
+                shipDateValue,
+                unitsSoldValue,
+                unitPriceValue,
+                unitCostValue,
+                totalRevenueValue,
+                totalCostValue,
+                totalProfitValue
+            );
+    }
+}
