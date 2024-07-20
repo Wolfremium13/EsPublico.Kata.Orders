8f0f08bd500950b59aef2cc13c4ab675e836f65a
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-07-20 10:21:59 +0200

    ✨ units sold domain logic

---
+++ EsPublico.Kata.Orders.Tests/Domain/Order/UnitsSoldShould.cs
@@ -0,0 +1,32 @@
+using EsPublico.Kata.Orders.Domain;
+using EsPublico.Kata.Orders.Domain.Order;
+using FluentAssertions;
+
+namespace EsPublico.Kata.Orders.Tests.Domain.Order;
+
+public class UnitsSoldShould
+{
+    [Fact]
+    public void BeCreated()
+    {
+        const long aUnitsSold = 10;
+        var validUnitsSold = UnitsSold.Create(aUnitsSold);
+
+        validUnitsSold.Match(
+            value => value.Value.Should().Be(aUnitsSold),
+            error => error.Should().BeNull()
+        );
+    }
+
+    [Fact]
+    public void NotAllowNegativeValues()
+    {
+        var invalidUnitsSold = UnitsSold.Create(-1);
+
+        invalidUnitsSold.Match(
+            value => value.Should().BeNull(),
+            error => error.Should().BeOfType<InvalidParameter>()
+                .Which.Message.Should().Be("Units sold must be greater or equal to 0")
+        );
+    }
+}
---
+++ EsPublico.Kata.Orders/Domain/Order/UnitsSold.cs
@@ -0,0 +1,19 @@
+using LanguageExt;
+
+namespace EsPublico.Kata.Orders.Domain.Order;
+
+public class UnitsSold
+{
+    private UnitsSold(long givenUnits) => Value = givenUnits;
+    public long Value { get; }
+
+    public static Either<Error, UnitsSold> Create(long givenUnits)
+    {
+        if (givenUnits < 0)
+        {
+            return new InvalidParameter("Units sold must be greater or equal to 0");
+        }
+
+        return new UnitsSold(givenUnits);
+    }
+}
