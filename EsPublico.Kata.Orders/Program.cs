40403beb2cf88309781800b19c82f63d3c86366e
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-07-21 13:27:44 +0100

    ✨ injection to use the program

--- EsPublico.Kata.Orders/Program.cs
+++ EsPublico.Kata.Orders/Program.cs
@@ -1,4 +1,9 @@
-﻿using EsPublico.Kata.Orders.Infrastructure.Config;
+﻿using EsPublico.Kata.Orders.Application;
+using EsPublico.Kata.Orders.Infrastructure.Apis;
+using EsPublico.Kata.Orders.Infrastructure.Config;
+using EsPublico.Kata.Orders.Infrastructure.Databases;
+using EsPublico.Kata.Orders.Infrastructure.Generators;
+using EsPublico.Kata.Orders.Infrastructure.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -15,8 +20,26 @@ configuration.GetSection("ApiSettings").Bind(apiSettings);
 
 // Logging
 var loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
-
 var logger = loggerFactory.CreateLogger<Program>();
-logger.LogInformation("Application started");
 
-logger.LogInformation("Application stopped");
+// Injection
+logger.LogInformation("Application starting...");
+
+var postgresAdapter = new PostgresAdapter(databaseSettings);
+var ordersRepository =
+    new PostgresOrdersRepository(postgresAdapter, loggerFactory.CreateLogger<PostgresOrdersRepository>());
+var localFileSystemRepository = new LocalFilesRepository(loggerFactory.CreateLogger<LocalFilesRepository>());
+var httpClientsFactory = new HttpClientFactory();
+var apiOrders = new OrdersHttpApi(httpClientsFactory, apiSettings, loggerFactory.CreateLogger<OrdersHttpApi>());
+var dateTimeGenerator = new DateTimeGenerator();
+var ordersService = new OrdersService(
+    ordersRepository,
+    localFileSystemRepository,
+    apiOrders,
+    dateTimeGenerator,
+    loggerFactory.CreateLogger<OrdersService>());
+
+// Application
+logger.LogInformation("Application started.");
+await ordersService.Ingest();
+logger.LogInformation("Application finished.");
