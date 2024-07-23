1281aa45faf304cfb796d847fceb2a941eaa34f8
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-07-23 06:21:27 +0100

    ♻️ improve legibility

--- EsPublico.Kata.Orders/Application/OrdersService.cs
+++ EsPublico.Kata.Orders/Application/OrdersService.cs
@@ -4,67 +4,61 @@ using EsPublico.Kata.Orders.Infrastructure.Generators;
 using EsPublico.Kata.Orders.Infrastructure.Repositories;
 using LanguageExt;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
-namespace EsPublico.Kata.Orders.Application;
-
-public class OrdersService(
-    OrdersRepository ordersRepository,
-    FilesRepository filesRepository,
-    OrdersApi ordersApi,
-    DateTimeGenerator dateTimeGenerator,
-    ILogger<OrdersService> logger)
+namespace EsPublico.Kata.Orders.Application
 {
-    public async Task Ingest()
+    public class OrdersService(
+        OrdersRepository ordersRepository,
+        FilesRepository filesRepository,
+        OrdersApi ordersApi,
+        DateTimeGenerator dateTimeGenerator,
+        ILogger<OrdersService> logger)
     {
-        var executionDate = dateTimeGenerator.Now();
-        var ordersExecutionCounter = 0L;
-        logger.LogInformation($"Ingesting orders for {executionDate}");
-        var maybeFirstOrders = await FirstOrdersCall(executionDate);
-        if (maybeFirstOrders.IsLeft)
+        public async Task Ingest()
         {
-            var errorMessage = maybeFirstOrders.LeftAsEnumerable().First().Message;
-            throw new Exception($"Error ingesting first orders: {errorMessage}");
-        }
-
-        var orders = maybeFirstOrders.RightAsEnumerable().First();
-        ordersExecutionCounter += orders.Value.Count;
-        logger.LogInformation($"Ingested {ordersExecutionCounter} orders");
-        while (orders is OrdersWithNextPage ordersWithNextPage)
-        {
-            var nextOrders = await NextOrdersCall(executionDate, ordersWithNextPage.NextOrdersLink);
-            if (nextOrders.IsLeft)
+            var executionDate = InitializeExecutionDate();
+            var maybeOrders = await ProcessOrders(executionDate, ordersApi.Get);
+            if (maybeOrders.IsLeft)
             {
-                throw new Exception(
-                    $"Error ingesting orders in page {
-                        ordersWithNextPage.NextOrdersLink}: {nextOrders.LeftAsEnumerable().First().Message}");
+                var errorMessage = maybeOrders.LeftAsEnumerable().First().Message;
+                throw new Exception($"Error ingesting first orders: {errorMessage}");
             }
-
-            orders = nextOrders.RightAsEnumerable().First();
-            ordersExecutionCounter += orders.Value.Count;
+            var orders = maybeOrders.RightAsEnumerable().First();
+            var ordersExecutionCounter = orders.Value.Count;
             logger.LogInformation($"Ingested {ordersExecutionCounter} orders");
+            while (orders is OrdersWithNextPage ordersWithNextPage)
+            {
+                maybeOrders = await ProcessOrders(executionDate, () => ordersApi.Get(ordersWithNextPage.NextOrdersLink));
+                if (maybeOrders.IsLeft)
+                {
+                    var errorMessage = maybeOrders.LeftAsEnumerable().First().Message;
+                    throw new Exception($"Error ingesting orders in page {ordersWithNextPage.NextOrdersLink}: {errorMessage}");
+                }
+
+                orders = maybeOrders.RightAsEnumerable().First();
+                ordersExecutionCounter += orders.Value.Count;
+                logger.LogInformation($"Ingested {ordersExecutionCounter} orders");
+            }
+            logger.LogInformation("Ingestion finished");
         }
 
-        logger.LogInformation("Ingestion finished");
-    }
+        private DateTime InitializeExecutionDate()
+        {
+            var executionDate = dateTimeGenerator.Now();
+            logger.LogInformation($"Ingesting orders for {executionDate}");
+            return executionDate;
+        }
 
-    private async Task<Either<Error, Domain.Orders>> FirstOrdersCall(DateTime executionDate)
-    {
-        return await (
-            from orders in ordersApi.Get().ToAsync()
-            from _ in ordersRepository.Save(orders.Value).ToAsync()
-            from __ in filesRepository.Save(orders.Value, executionDate).ToAsync()
-            select orders
-        ).ToEither();
-    }
-
-    private async Task<Either<Error, Domain.Orders>> NextOrdersCall(DateTime executionDate,
-        NextOrdersLink nextOrdersLink)
-    {
-        return await (
-            from orders in ordersApi.Get(nextOrdersLink).ToAsync()
-            from _ in ordersRepository.Save(orders.Value).ToAsync()
-            from __ in filesRepository.Save(orders.Value, executionDate).ToAsync()
-            select orders
-        ).ToEither();
+        private async Task<Either<Error, Domain.Orders>> ProcessOrders(DateTime executionDate, Func<Task<Either<Error, Domain.Orders>>> getOrdersFunc)
+        {
+            var maybeOrders = await (
+                from orders in getOrdersFunc().ToAsync()
+                from _ in ordersRepository.Save(orders.Value).ToAsync()
+                from __ in filesRepository.Save(orders.Value, executionDate).ToAsync()
+                select orders
+            );
+            return maybeOrders;
+        }
     }
 }
